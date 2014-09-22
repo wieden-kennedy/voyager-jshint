4315d405e272763efe318b8d95d7f32afb247431
David Glivar <davidglivar@gmail.com>
2014-09-22 16:59:03 -0700

    fixed globs

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var jshint = require('gulp-jshint');
 module.exports = function (voyager) {
 
   voyager.task('read', 'jshint', function (done) {
-    this.src(['javascripts/**/*.js', '!javascripts/vendor/*'])
+    this.src(['javascripts/**/*.js', '!javascripts/vendor/**'])
       .pipe(jshint())
       .on('end', done)
       .pipe(jshint.reporter('jshint-stylish'));
   });
 
-  voyager.watch(['javascripts/**/*.js', '!javascripts/vendor/*'], 'jshint');
+  voyager.watch(['javascripts/**/*.js', '!javascripts/vendor/**'], 'jshint');
 };
